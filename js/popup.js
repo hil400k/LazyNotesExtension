5cbe546b7fbdbc7f4a532d35a8465207114cfa33
Дмитро Хільчук < >
2016-05-22 15:59:55 +0300

    built project

---
+++ js/popup.js
@@ -0,0 +1,10 @@
+angular.module("app", ['ui.bootstrap'])
+.config(function() {
+    console.info('hello');
+})
+.controller("MainController", function() {
+    
+})
+.controller("ListController", function() {
+    
+})
