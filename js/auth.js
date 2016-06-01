aad69c3658153113414578b898d7d7b91a2061fb
Дмитро Хільчук < >
2016-06-01 23:39:32 +0300

    login improvings

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,11 @@
 (function(){
-    window.onhashchange = function() {
-        var userObject = localStorage['firebase:session::lazynotes'] || null,
-            settings = localStorage['settings'] || null;
+    document.addEventListener("logged-in", function(data) {
+      var userObject = localStorage['firebase:session::lazynotes'] || null,
+        settings = localStorage['settings'] || null;
 
         chrome.storage.local.set({'firebase:session::lazynotes': userObject});
         chrome.storage.local.set({'settings': settings});
 
         chrome.runtime.sendMessage('user-setted');
-    };
+    });
 }());
